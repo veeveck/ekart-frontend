65327a33bd643d83c7e075aa0ed2920ef1fa0ed4
Vivek <veeveckjeet@gmail.com>
2024-02-02 13:14:15 -0500

    Login register routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from "react";
 //import { Counter } from "./features/counter/Counter";
 import ProductList from "./features/productList/ProductList";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
 
 import "./App.css";
+import SignupPage from "./pages/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
